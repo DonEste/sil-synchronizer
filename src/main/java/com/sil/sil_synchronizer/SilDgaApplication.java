6b3d0952545b6c2d41c4596ca10a5ec3b842816b
Matias Carreño <mcarreno@myhotel.cl>
2022-11-13 22:41:54 -0300

    Blank Project Init

---
+++ src/main/java/com/sil/sil_synchronizer/SilDgaApplication.java
@@ -0,0 +1,13 @@
+package com.sil.sil_synchronizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilDgaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilDgaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sil/sil_synchronizer/SilDgaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sil.sil_synchronizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilDgaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
