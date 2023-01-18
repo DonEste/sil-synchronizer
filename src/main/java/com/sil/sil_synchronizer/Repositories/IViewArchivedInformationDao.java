72008b5e77abb97e88ad1e0fc935759a46182d29
Matias Carreño <murua.matias.carreno@gmail.com>
2023-01-18 00:07:40 -0300

    Query for obtaining inf improvement

--- src/main/java/com/sil/sil_synchronizer/Repositories/IViewArchivedInformationDao.java
+++ src/main/java/com/sil/sil_synchronizer/Repositories/IViewArchivedInformationDao.java
@@ -10,11 +10,24 @@ import java.util.List;
 
 public interface IViewArchivedInformationDao extends JpaRepository<ViewArchivedInformationEntity, Long> {
 
-    @Query(value = "SELECT ID, STA_SiteNumber, INF_NumberInStation, MAX(FORMAT(INF_Date, 'yyyy-MM-dd HH:00:00')) INF_Date, SUM(INF_Value) INF_Value " +
-            "FROM View_ArchivedInformations " +
+    //TODO: Test query with real data
+    @Query(value = "SELECT ID,  " +
+            "       STA_SiteNumber,  " +
+            "       INF_NumberInStation,  " +
+            "       MAX(FORMAT(INF_Date, 'yyyy-MM-dd HH:00:00')) INF_Date,  " +
+            "       CASE INF_NumberInStation when :flowNumber THEN AVG(INF_Value) ELSE 0 END as INF_Flow,  " +
+            "       CASE INF_NumberInStation when :totalizerNumber THEN SUM(INF_Value) ELSE 0 END as INF_Totalizer,  " +
+            "       CASE INF_NumberInStation when :pheaticLevel THEN AVG(INF_Value) ELSE 0 END as INF_PhreaticLevel  " +
+            "FROM View_ArchivedInformations  " +
             "WHERE INF_NumberInStation in (:informationNumbers)  " +
-            "AND INF_Date > :startDate " +
-            "GROUP BY FORMAT(INF_Date, 'yyyy-MM-dd HH'), STA_SiteNumber, INF_NumberInStation, ID", nativeQuery = true)
-    List<ViewArchivedInformationEntity> findLastByInformationNumber(@Param("informationNumbers") List<Long> informationNumbers,
-                                                                    @Param("startDate") Date startDate);
+            "  AND INF_Date > :startDate  " +
+            "  AND INF_Date < DATEADD(hour, -:offsetHours, SYSDATETIMEOFFSET())  " +
+            "GROUP BY FORMAT(INF_Date, 'yyyy-MM-dd HH'), STA_SiteNumber, INF_NumberInStation, ID  " +
+            "HAVING count(*) > :minDataSamples", nativeQuery = true)
+    List<ViewArchivedInformationEntity> findHourlyAverage(@Param("informationNumbers") List<Long> informationNumbers,
+                                                          @Param("startDate") Date startDate,
+                                                          @Param("flowNumber") Long flowNumber,
+                                                          @Param("totalizerNumber") Long totalizerNumber,
+                                                          @Param("pheaticLevel") Long pheaticLevel,
+                                                          @Param("offsetHours") int offsetHours);
 }
